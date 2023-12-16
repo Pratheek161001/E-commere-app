30ab96c50a3cb48b11e3b63ee08cb7f1dc04f033
Pratheek <pratheeksk70@gmail.com>
2023-12-17 01:14:43 +0530

    cart has the items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,23 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Example() {
-    
+    const CartItems = [
+        {
+            title: 'Colors',
+            price: 100,
+            imageUrl: require("../assets/Album 1.png")},
+        {
+            title: 'Black and white Colors',
+            price: 50,
+            imageUrl: require("../assets/Album 2.png")},
+        {
+            title: 'Yellow and Black Colors',
+            price: 70,
+            imageUrl: require("../assets/Album 3.png")},
+        ]
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -19,8 +33,18 @@ function Example() {
           <Offcanvas.Title>Your Cart</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+        <ul>
+        <div className="d-flex justify-content-start ">
+            <li style={{listStyleType:'none'}}>
+                {CartItems.map((product,index)=>(
+                <Card key={index} style={{ width: '6rem', backgroundColor:'beige' }} className='p-1 m-2'>
+                    <Card.Body className='p-0'><Card.Img variant="top" src={product.imageUrl} />{product.title} ${product.price} <Button variant="danger">remove</Button></Card.Body>
+                    <Card.Text></Card.Text>   
+                </Card>
+                ))}
+            </li>
+        </div>
+            </ul>
         </Offcanvas.Body>
       </Offcanvas>
     </>
