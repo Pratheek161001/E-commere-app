f7cc97a965346f4031bcf3469260410183a89d53
Pratheek <pratheeksk70@gmail.com>
2023-12-16 20:21:58 +0530

    navbar done

---
+++ src/Applogo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Applogo = () => {
+  return (
+    <div>The Generics</div>
+  )
+}
+
+export default Applogo
---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Minuz-Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#shop">shop</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Button variant="primary" className="mr-3">Cart 0</Button>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
